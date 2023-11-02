c7a447cc13fe9022cf48e31cb61b2b0befe1ebe4
CallMeCJUnderscore <monsterboy09@gmail.com>
2023-11-02 14:13:51 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+
+    /*---------------VARIABLES---------------*/
+
+    private int vin;
+    private int year;
+    private String make;
+    private String model;
+    private String vehicleType;
+    private String color;
+    private int odometer;
+    private double price;
+
+    /*--------------CONSTRUCTORS-------------*/
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+    /*------------GETTERS/SETTERS------------*/
+
+    //CODE HERE
+
+    /*---------------FUNCTIONS---------------*/
+
+    //CODE HERE
+
+}
